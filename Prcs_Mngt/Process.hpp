b437041104af98c6d945ae7762da140562cf0efc
pataliaferro0329 <pataliaferro0329@gmail.com>
2020-01-27 10:19:50 -0500

    First Commit

---
+++ Prcs_Mngt/Process.hpp
@@ -0,0 +1,12 @@
+//Author:   Patrick Taliaferro
+//Date:     01/26/2020
+//Purpose:  Provide the C++ header for a Process Object
+
+#define TRUST_LIMIT 10 //Have higher trust levels 
+                       //coincide with greater privileges
+
+#define TRUST_TYPES {    INFORMATION_ACCESS,
+			 AUTHORITY_OVER_OTHERS,
+			 ABILITY_TO_REPRODUCE     }
+
+			 
